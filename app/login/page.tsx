ba11202f73d303f4f936519c0573c45a830f26c5
Abhishek Saral <abhisheksaral17@gmail.com>
2024-05-29 21:31:58 -0700

    shadcn added

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,35 @@
-export default function about() {
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export default function LoginForm() {
   return (
-    <main>
-      <h1>Hello World</h1>
+    <main className=" min-h-screen flex flex-col items-center justify-center">
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-2xl">Login</CardTitle>
+          <CardDescription>
+            Just click the button below to authenticate using Passkeys without
+            any Password or Usernames.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <Button className="w-full">Login with Passkey</Button>
+        </CardContent>
+        <CardFooter>
+          <p className="text-xs">
+            By creating an account and/or logging in, you agree to RevNotes
+            <span className="text-primary"> Terms of Service</span> and
+            <span className="text-primary"> Privacy Policy</span>.
+          </p>
+        </CardFooter>
+      </Card>
     </main>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <Link href={"login"}>About</Link>
+    <main className="min-h-screen flex flex-col items-center justify-center gap-6">
+      <h1 className=" text-5xl font-bold">The simplest way to keep notes</h1>
+      <p>
+        All your notes,synced to all your devices. Use RevNotes now in your
+        browser.
+      </p>
+      <Link href={"login"}>Open in Browser</Link>
     </main>
   );
 }
